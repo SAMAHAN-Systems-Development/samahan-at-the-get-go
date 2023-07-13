98e876a04e033c268015c2c2554af81d532dadd5
Migs <miguelemmanueldailisan@gmail.com>
2023-07-13 20:40:29 +0800

    feat: added fourth section

---
+++ src/components/ui/AboutFourthSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutFourthSection = () => {
+  return (
+    <>
+      <div className="relative flex flex-col justify-start w-full gap-2 m-5 xl:flex-row md:flex-col" />
+    </>
+  );
+};
+
+export default AboutFourthSection;
