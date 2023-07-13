d13d6062e96a83ae1a259bbeaf56219c9ebd137c
Migs <miguelemmanueldailisan@gmail.com>
2023-07-13 20:58:05 +0800

    fix: added components

--- src/components/ui/AboutFourthSection.tsx
+++ src/components/ui/AboutFourthSection.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import AboutForthSectionBlueCard from "@/components/ui/AboutPage/AboutForthSectionBlueCard";
+import AboutForthSectionOrangeCard from "@/components/ui/AboutPage/AboutForthSectionOrangeCard";
+
 const AboutFourthSection = () => {
   return (
     <>
-      <div className="relative flex flex-col justify-start w-full gap-2 m-5 xl:flex-row md:flex-col" />
+      <div className="relative flex flex-col justify-start w-full gap-2 m-5">
+        <AboutForthSectionBlueCard year="This 2023" />
+        <AboutForthSectionOrangeCard year="SAMAHAN that is at the get-go. " />
+      </div>
     </>
   );
 };
