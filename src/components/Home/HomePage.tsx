73dc1bb64690fe4db8d71b227ff3dca923e69c0b
Migs <miguelemmanueldailisan@gmail.com>
2023-07-13 15:00:45 +0800

    fix: restored home page

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,12 +1,12 @@
 import type { FC } from "react";
 import React from "react";
 
-import AboutForthSectionBlueCard from "@/components/ui/AboutPage/AboutForthSectionBlueCard";
+import Button from "@/components/ui/Button";
 
 const HomePage: FC = () => {
   return (
     <>
-      {/* <Button
+      <Button
         href="https://www.samahan.addu.edu.ph/"
         className="font-artega bg-beige text-white"
       >
@@ -17,8 +17,7 @@ const HomePage: FC = () => {
         className="font-appleGaramound bg-beige text-white"
       >
         Click Me!
-      </Button> */}
-      <AboutForthSectionBlueCard year="This 2023" />
+      </Button>
     </>
   );
 };
