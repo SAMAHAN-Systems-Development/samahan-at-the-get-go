0ecb467b566ac97a9618a734db9e434fee759c55
Innhahn <jaytan3825@gmail.com>
2023-07-14 22:19:32 +0800

    feat(OfficesDescriptionCard): addded sm media tag to make the component more responsive

--- src/components/ui/OfficesCardComponent.tsx
+++ src/components/ui/OfficesCardComponent.tsx
@@ -10,28 +10,30 @@ const OfficesCardComponent: React.FC<OfficesCardComponentProps> = ({
   officesDescription,
 }) => {
   return (
-    <div className="grid h-[18.6875rem] content-between mx-4 p-8 text-white bg-orange rounded-3xl md:h-[29.375rem] lg:h-[26.75rem] ">
+    <div className="grid h-[300px] content-between w-full p-8 text-white bg-orange border-solid border-card rounded-card sm:h-[320px] md:h-[470px] lg:h-[428px] ">
       <div className="flex flex-row text-sm w-full justify-between">
-        <p className="font-artega font-medium lg:text-[1.5625rem] "> 01 </p>
-        <p className="font-appleGaramound italic font-bold lg:text-[1.9375rem]">
+        <p className="font-artega font-medium text-[0.875rem] sm:text-[1rem] md:text-[1.25rem] lg:text-[1.5625rem]">
+          {" "}
+          01{" "}
+        </p>
+        <p className="font-appleGaramound italic font-bold text-[0.875rem] sm:text-[1.25rem] md:text-[1.46875rem] lg:text-[1.9375rem]">
           #AtTheGetGo
         </p>
       </div>
-      <div className="flex flex-col justify-self-center md:w-[24.5rem] lg:w-[56.0625rem]">
-        <h1 className="text-center font-artega text-[1.9375rem] font-medium justify-self-center mb-[24px] md:text-[3.8125rem] lg:text-[4.75rem]">
+      <div className="flex flex-col justify-self-center sm:w-[320px] md:w-[392px] lg:w-[897px]">
+        <h1 className="text-center font-artega  text-[1.9375rem] font-medium justify-self-center sm:text-[2.615rem] md:text-[3.8125rem] lg:text-[4.75rem]">
           {officesTitle}
         </h1>
-        <p className="font-sans flex-wrap text-justify flex text-sm md:text-[1.1875rem] lg:text-[1.5625rem] lg:text-center">
+        <p className="font-sans flex-wrap text-justify flex text-[0.875rem] sm:[1rem] md:text-[1.1875rem] lg:text-[1.5625rem] lg:text-center">
           {officesDescription}
         </p>
       </div>
       <div className="flex flex-row w-full justify-between">
-        <div className="w-[0.9375rem] h-[0.9375rem] rounded-full bg-blue md:w-[1.4375rem] md:h-[1.4375rem] lg:w-[1.4375rem] lg:h-[1.4375rem]">
+        <div className="rounded-full bg-blue w-[15px] h-[15px] sm:w-[17.5px] sm:h-[17.5px] md:w-[23px] md:h-[23px] lg:w-[23px] lg:h-[23px]">
           {" "}
         </div>
-        <p className="text-xs font-normal md:text-[1.4375rem] lg:text-[1.5625rem]">
-          {" "}
-          SAMAHAN{" "}
+        <p className="text-xs font-normal sm:text-[1rem] md:text-[1.4375rem] lg:text-[1.5625rem]">
+          SAMAHAN
         </p>
       </div>
     </div>
