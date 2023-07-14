eb265e7e7de9665fafae773719d56e6a2c5d698c
Innhahn <jaytan3825@gmail.com>
2023-07-14 21:29:21 +0800

    feat(OfficesTitleCard): added media tag sm to make the component more responsive

--- src/components/ui/OfficesTitleCardComponent.tsx
+++ src/components/ui/OfficesTitleCardComponent.tsx
@@ -11,12 +11,12 @@ const OfficesTitleCardComponent: React.FC<OfficesTitleCardComponentProps> = ({
   officesPosition,
 }) => {
   return (
-    <div className="grid h-64 content-between mx-4 p-4 text-white bg-transparent rounded-3xl border-2 border-blue md:h-[38.3125rem] md:p-8 lg:h-[651px] lg:p-16">
+    <div className="grid h-[262px] p-4 w-full content-between text-lightBlue bg-transparent rounded-card border-card border-lightBlue sm:h-[400px] md:h-[622px] md:p-8 lg:h-[738px] lg:p-16">
       <div className="flex flex-row w-full justify-between">
-        <p className="font-artega font-medium text-blue text-sm md:text-xl lg:text-2xl">
+        <p className="font-artega font-medium text-sm sm:text-l md:text-xl lg:text-2xl">
           00
         </p>
-        <div className="text-blue relative h-14 w-8 md:w-[3.3125rem] md:h-[5.357rem] lg:w-[3.3125rem] lg:h-[5.357rem]">
+        <div className="relative h-[52px] w-[32px] sm:h-[62px] sm:w-[41px] md:w-[53px] md:h-[86px] lg:w-[53px] lg:h-[86px]">
           <Image
             alt="AtTheGetGoLogo"
             src={SamahanAtTheGetGoLogo}
@@ -25,11 +25,11 @@ const OfficesTitleCardComponent: React.FC<OfficesTitleCardComponentProps> = ({
           />
         </div>
       </div>
-      <div className="flex flex-col text-blue w-full">
-        <h2 className="font-appleGaramond italic text-2xl font-bold md:text-[3.0625rem] md:mb-5 lg:text-[4.75rem] lg:mb-20">
+      <div className="flex flex-col w-full">
+        <p className="font-appleGaramond italic font-bold text-[1.5625rem] sm:text-[2.1rem] md:text-[3.0625rem] lg:text-[4.75rem]">
           Office of the
-        </h2>
-        <h1 className="font-artega text-3xl font-medium md:text-[3.8125rem] lg:text-[7.4375rem]">
+        </p>
+        <h1 className="font-artega font-medium text-[1.75rem] sm:text-[2.5rem] md:text-[3.8125rem] lg:text-[7.4375rem]">
           {officesPosition}
         </h1>
       </div>
