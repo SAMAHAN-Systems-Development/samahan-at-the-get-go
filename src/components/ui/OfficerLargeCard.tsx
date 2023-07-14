93986ce20d930e0f0d21ea0baa2c1583d4edc236
iyabc <juliennepanes@gmail.com>
2023-07-14 22:31:11 +0800

    refactor: change interface to type, added isButton prop, removed link prop

--- src/components/ui/OfficerLargeCard.tsx
+++ src/components/ui/OfficerLargeCard.tsx
@@ -1,40 +1,47 @@
 import React from "react";
 import Image from "next/image";
 
-interface OfficerLargeCardProps {
+type OfficerLargeCardProps = {
   firstName: string;
   lastName: string;
   position: string;
   imageUrl: string;
-  link: string;
-}
+  isButton: boolean;
+};
 
 const OfficerCard: React.FC<OfficerLargeCardProps> = ({
   firstName,
   lastName,
   position,
   imageUrl,
-  link,
+  isButton,
 }) => {
   return (
-    <a
-      href={link}
-      className="h-64 relative block overflow-hidden bg-blue max-w-2xl rounded-[15px] shadow lg:h-72 hover:cursor-pointer"
-    >
+    <div className="h-64 relative block overflow-hidden bg-blue max-w-2xl rounded-[15px] shadow lg:h-72">
       <Image
         src={imageUrl}
         alt={lastName}
         className="object-cover object-top"
         fill
       />
-      <div className="absolute bottom-0 left-0 flex items-end text-white w-full h-full p-4 lg:p-6 group/overlay hover:bg-blue/25 transition-all ease-in-out duration-300">
+      <div
+        className={[
+          [
+            isButton &&
+              "group/overlay hover:bg-blue/25 transition-all ease-in-out duration-300",
+          ],
+          [
+            "absolute bottom-0 left-0 flex items-end text-white w-full h-full p-4 lg:p-6",
+          ],
+        ].join(" ")}
+      >
         <div className="text-shadow text-xl leading-6 sm:text-[1.5rem] sm:leading-7 md:text-title-sm md:leading-title-sm lg:text-title-md lg:leading-title-md group-hover/overlay:translate-x-3 transition-all ease-in-out duration-300">
           <p className="font-appleGaramond-bold italic">{firstName}</p>
           <p className="font-artega uppercase">{lastName}</p>
           <p className="font-sans uppercase">{position}</p>
         </div>
       </div>
-    </a>
+    </div>
   );
 };
 
