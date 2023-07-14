4867c2557db5576c4c133cfbfced665cf0b23d16
Migs <miguelemmanueldailisan@gmail.com>
2023-07-14 21:49:15 +0800

    fix: fixed responsiveness

--- src/components/ui/AboutPage/AboutPageSections/AboutFirstSection.tsx
+++ src/components/ui/AboutPage/AboutPageSections/AboutFirstSection.tsx
@@ -8,17 +8,17 @@ const AboutFirstSection = (props: TitleProps) => {
   return (
     <>
       <div className="w-full h-screen flex justify-center">
-        <div className="relative w-full m-5 border-2 border-blue rounded-lg flex flex-col">
-          <div className="flex flex-row justify-between m-14">
-            <p className="text-blue font-light sm:text-xl text-sm font-artega">
+        <div className="relative w-full m-5 border-card border-blue rounded-section-card flex flex-col">
+          <div className="flex flex-row justify-between sm:m-14 m-6">
+            <p className="text-blue font-light sm:text-xl text-xs font-artega">
               About us
             </p>
-            <p className="text-blue font-semibold sm:text-2xl text-sm italic font-appleGaramound">
+            <p className="text-blue font-semibold sm:text-2xl text-xs italic font-appleGaramound">
               #AtTheGetGo
             </p>
           </div>
 
-          <div className="absolute flex flex-col bottom-0 m-14">
+          <div className="absolute flex flex-col bottom-0 sm:m-14 m-6">
             <h1 className="text-blue md:text-4xl lg:text-7xl text-xl font-appleGaramound italic font-bold">
               THIS is
             </h1>
