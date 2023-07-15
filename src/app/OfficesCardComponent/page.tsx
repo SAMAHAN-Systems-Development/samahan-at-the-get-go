6725aba9f231584163b535d024a4392781c1cd89
Innhahn <jaytan3825@gmail.com>
2023-07-15 22:46:31 +0800

    feat(OfficesDescriptionCard): created another route for pull request checking

---
+++ src/app/OfficesCardComponent/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import OfficesCardComponent from "@/components/ui/OfficesCardComponent";
+
+const page = () => {
+  return (
+    <div>
+      <OfficesCardComponent
+        officesTitle="The OSP"
+        officesDescription="is the highest executive office, aims to establish harmony and efficiency in both the internal operations of the student government and external relations."
+      />
+    </div>
+  );
+};
+
+export default page;
