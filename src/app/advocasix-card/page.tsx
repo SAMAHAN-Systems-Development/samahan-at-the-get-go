f46e19037bae65343a8f3cc5677a0531d65e69d7
Angelli <akmalkuino@addu.edu.ph>
2023-07-16 06:21:35 +0800

    refactor: add p tag and dir in app dir

---
+++ src/app/advocasix-card/page.tsx
@@ -0,0 +1,9 @@
+import AdvocasixDescriptionCard from '@/components/ui/Advocasix/AdvocasixDescriptionCard';
+
+export default function page() {
+  return (
+    <div className="p-6">
+      <AdvocasixDescriptionCard />
+    </div>
+  );
+}
--- src/components/ui/Advocasix/AdvocasixDescriptionCard.tsx
+++ src/components/ui/Advocasix/AdvocasixDescriptionCard.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
-import arrow from "public/assets/images/orangeArrow1.png";
+import arrow from 'public/assets/images/orangeArrow1.png';
 
 const AdvocasixDescriptionCard = () => {
   return (
     <>
       <div className="w-full bg-blue rounded-section-card flex flex-col justify-end px-8 py-10 md:px-12 md:py-14 min-h-[18rem] md:h-[32] lg:h-[41.25rem]">
         <div className="flex h-auto mb-auto">
-          <div className="font-artega text-white text-base lg:text-title-sm mr-auto">
+          <p className="font-artega text-white text-base lg:text-title-sm mr-auto">
             ADVOCASIX
-          </div>
+          </p>
           <div className="relative w-64 ml-3">
             <Image
               alt="/"
               src={arrow}
               fill={true}
-              style={{ objectFit: "contain" }}
+              style={{ objectFit: 'contain' }}
             />
           </div>
         </div>
-        <div className="text-white text-base lg:text-2xl">
+        <p className="text-white text-base lg:text-2xl">
           The Samahan ng mga Mag-aaral ng Pamantasan ng Ateneo de Davao, guided
           by its Jesuit, Filipino, and Mindanaoan values, aspires to be an
           autonomous student government that is genuinely present in championing
           here and now its chosen six pillar advocacies.
-        </div>
+        </p>
       </div>
     </>
   );
