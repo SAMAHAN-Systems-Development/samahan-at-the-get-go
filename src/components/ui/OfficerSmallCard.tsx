ae9a4da5dc68ae9266c7f27508fd877f27b537e3
louispawaon <lpawaon@gmail.com>
2023-07-15 13:49:09 +0800

    fix: adjustments for different viewports in image and name

--- src/components/ui/OfficerSmallCard.tsx
+++ src/components/ui/OfficerSmallCard.tsx
@@ -18,18 +18,20 @@ const OfficerSmallCard: React.FC<OfficerSmallCard> = ({
 }) => {
   return (
     <div className="flex flex-col items-center gap-y-5">
-      <Image src={imageUrl} alt={lastName} width={148} height={163} />
+      <div className="h-[163px] md:h-[187px] w-[148px] md:w-[171px] relative rounded-card">
+        <Image src={imageUrl} alt={lastName} fill />
+      </div>
       <div className="flex flex-col items-center">
-        <div className="space-y-0 flex flex-col items-center">
-          <h2 className="text-[15px] text-[#1A275F] font-appleGaramound font-semibold italic">
+        <div className=" flex flex-col items-center">
+          <h2 className="text-[13px] md:text-[16px] text-[#1A275F] font-appleGaramond font-bold italic text-shadow-soft leading-5">
             {firstName}
           </h2>
-          <h2 className="text-xl font-artega text-[#1A275F] uppercase">
+          <h2 className="text-[16px] md:text-[20px] font-artega text-[#1A275F] uppercase leading-5 font-medium">
             {lastName}
           </h2>
         </div>
       </div>
-      <div className="flex-col flex-shrink-0 flex max-w-[193px] max-h-[30px] text-[#263167] text-[13px] font-normal text-center">
+      <div className="flex-col flex-shrink-0 flex max-w-[193px] max-h-[30px] text-[#263167] text-[13px] font-normal text-center leading-normal">
         {department ? (
           <>
             <span className="font-bold">{position}</span>
