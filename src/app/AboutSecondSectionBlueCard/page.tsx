9eb80f6d3993c0a50e449e9e544c555f9c441842
Migs <miguelemmanueldailisan@gmail.com>
2023-07-15 23:25:33 +0800

    chore: added app

---
+++ src/app/AboutSecondSectionBlueCard/page.tsx
@@ -0,0 +1,9 @@
+import AboutSecondSectionBlueCard from "@/components/ui/AboutPage/AboutSecondSection/AboutSecondSectionBlueCard";
+
+export default function Home() {
+  return (
+    <main>
+      <AboutSecondSectionBlueCard title="This Year" />
+    </main>
+  );
+}
