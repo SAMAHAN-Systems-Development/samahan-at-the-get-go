86e7403d4e4474e447740d363c2d552028c0faab
Migs <miguelemmanueldailisan@gmail.com>
2023-07-15 23:10:09 +0800

    chore: added app

---
+++ src/app/AboutSecondSectionWhiteCard/page.tsx
@@ -0,0 +1,12 @@
+import AboutSecondSectionWhiteCard from "@/components/ui/AboutPage/AboutSecondSection/AboutSecondSectionWhiteCard";
+
+export default function Home() {
+  return (
+    <main>
+      <AboutSecondSectionWhiteCard
+        first_header="Commitment"
+        second_header="Navigate"
+      />
+    </main>
+  );
+}
