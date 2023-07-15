32678a7e65fdb1960bc5b3ec5a61649147d20e90
louispawaon <lpawaon@gmail.com>
2023-07-15 23:34:39 +0800

    fix: added component view on app folder

---
+++ src/app/see-below-component/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SeeBelow from "@/components/ui/Hero/SeeBelow";
+
+const page = () => {
+  return (
+    <div>
+      <SeeBelow />
+    </div>
+  );
+};
+
+export default page;
