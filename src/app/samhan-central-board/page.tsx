26558ffaddf0a7f38b57f8781d34c3ccd3ff0693
iyabc <juliennepanes@gmail.com>
2023-07-16 14:56:17 +0800

    feat(SCBTitleSection): initial commit, added page for SCB

---
+++ src/app/samhan-central-board/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SCBTitleSection from '@/components/SCB/SCBTitleSection';
+
+const page = () => {
+  return (
+    <div className="px-4 ">
+      <SCBTitleSection />
+    </div>
+  );
+};
+
+export default page;
