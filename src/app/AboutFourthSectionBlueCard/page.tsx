bf73026b69cfa309885b438f40c797e0b0a2c66c
Migs <miguelemmanueldailisan@gmail.com>
2023-07-17 17:17:44 +0800

    refactor: updated component

--- src/app/AboutFourthSectionBlueCard/page.tsx
+++ src/app/AboutFourthSectionBlueCard/page.tsx
@@ -1,9 +1,9 @@
-import AboutForthSectionBlueCard from "@/components/ui/AboutPage/AboutFourthSection/AboutForthSectionBlueCard";
+import AboutForthSectionBlueCard from '@/components/ui/AboutPage/AboutFourthSection/AboutForthSectionBlueCard';
 
 export default function Home() {
   return (
     <main>
-      <AboutForthSectionBlueCard year="This 2023" />
+      <AboutForthSectionBlueCard />
     </main>
   );
 }
