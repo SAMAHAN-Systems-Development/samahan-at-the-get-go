9d476b60861ee5d76f4ba63304aab38cf1813a21
Angelli <akmalkuino@addu.edu.ph>
2023-07-17 10:10:11 +0800

    refactor: adjusted padding and text size

--- src/components/ui/Advocasix/AdvocasixHeaderCard.tsx
+++ src/components/ui/Advocasix/AdvocasixHeaderCard.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
-import logo from "public/assets/images/logo.png";
+import logo from 'public/assets/images/logo.png';
 
 const AdvocasixHeaderCard = () => {
   return (
     <>
-      <div className="w-full bg-orange rounded-section-card flex flex-col justify-end px-8 py-10 md:px-12 md:py-14 h-72 md:h-[32] lg:h-[41.25rem]">
+      <div className="w-full bg-orange rounded-section-card flex flex-col justify-end px-8 py-10 md:px-12 md:py-14 lg:px-20 lg:py-20 h-72 md:h-[32] lg:h-[41.25rem]">
         <div className="relative w-6 h-12 md:w-10 md:h-16 mb-auto self-end">
           <Image
             alt="/"
             src={logo}
             fill={true}
-            style={{ objectFit: "contain" }}
+            style={{ objectFit: 'contain' }}
           />
         </div>
-        <p className="font-appleGaramound italic text-white text-xl lg:text-title-md">
+        <p className="font-appleGaramound italic text-white text-xl md:text-3xl lg:text-title-md">
           Six Pillar Advocacies
         </p>
-        <p className="font-artega text-white text-title-md lg:text-title-lg">
+        <p className="font-artega text-white text-title-md md:text-6xl lg:text-title-lg leading-title-sm lg:leading-title-lg">
           We Are:
         </p>
       </div>
