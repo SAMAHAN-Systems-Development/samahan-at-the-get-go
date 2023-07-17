d4b1f0c15460cd469055cc623d28dfc94c8b9d32
Angelli <akmalkuino@addu.edu.ph>
2023-07-17 09:38:16 +0800

    refactor: adjusted padding and arrow dimensions

--- src/components/ui/Advocasix/AdvocasixDescriptionCard.tsx
+++ src/components/ui/Advocasix/AdvocasixDescriptionCard.tsx
@@ -6,18 +6,13 @@ import arrow from 'public/assets/images/orangeArrow1.png';
 const AdvocasixDescriptionCard = () => {
   return (
     <>
-      <div className="w-full bg-blue rounded-section-card flex flex-col justify-end px-8 py-10 md:px-12 md:py-14 min-h-[18rem] md:h-[32] lg:h-[41.25rem]">
-        <div className="flex h-auto mb-auto">
-          <p className="font-artega text-white text-base lg:text-title-sm mr-auto">
+      <div className="w-full bg-blue rounded-section-card flex flex-col justify-end px-8 py-10 md:px-12 md:py-14 lg:px-20 lg:py-24 min-h-[18rem] md:h-[32] lg:h-[41.25rem]">
+        <div className="flex mb-auto">
+          <p className="font-artega leading-normal text-white text-base md:text-2xl lg:text-title-sm mr-auto">
             ADVOCASIX
           </p>
-          <div className="relative w-64 ml-3">
-            <Image
-              alt="/"
-              src={arrow}
-              fill={true}
-              style={{ objectFit: 'contain' }}
-            />
+          <div className="relative w-[268px] ml-3 justify-end">
+            <Image alt="/" src={arrow} fill style={{ objectFit: 'contain' }} />
           </div>
         </div>
         <p className="text-white text-base lg:text-2xl">
