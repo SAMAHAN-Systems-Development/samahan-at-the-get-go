de73195c4cf6d1ce6289f0707af79bd28717decd
Migs <miguelemmanueldailisan@gmail.com>
2023-07-17 16:17:57 +0800

    fix: fixed error

---
+++ src/stories/components/About/AboutSecontSection.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import AboutSecondSection from '@/components/ui/AboutPage/AboutSecondSection/AboutSecondSection';
+
+const meta: Meta<typeof AboutSecondSection> = {
+  title: 'About/ About Second Section',
+  component: AboutSecondSection,
+};
+
+export default meta;
+type Story = StoryObj<typeof AboutSecondSection>;
+
+export const Primary: Story = {
+  render: () => <AboutSecondSection />,
+};
