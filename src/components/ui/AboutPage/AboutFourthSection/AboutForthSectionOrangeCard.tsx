d9d59059f9844f2f67d25e380aeaef4d79031083
Migs <miguelemmanueldailisan@gmail.com>
2023-07-17 17:40:38 +0800

    refactor: fixed styles

--- src/components/ui/AboutPage/AboutFourthSection/AboutForthSectionOrangeCard.tsx
+++ src/components/ui/AboutPage/AboutFourthSection/AboutForthSectionOrangeCard.tsx
@@ -3,17 +3,15 @@ import React from 'react';
 const AboutForthSectionOrangeCard = () => {
   return (
     <>
-      <div className="w-full bg-orange rounded-lg flex flex-col">
-        <div className="flex flex-col justify-center py-7">
-          <div className="flex mx-12 my-4">
-            <p className="text-white font-normal text-justify lg:text-lg md:text-base text-sm">
-              But this time, as we move forward to reclaim the life we used to
-              own, we envision a community that starts anew, a student body that
-              expands to new directions, and a
-            </p>
-          </div>
+      <div className="w-full bg-orange rounded-lg flex flex-col p-14">
+        <div className="flex flex-col justify-center">
+          <p className="text-white font-normal text-justify lg:text-lg md:text-base text-sm">
+            But this time, as we move forward to reclaim the life we used to
+            own, we envision a community that starts anew, a student body that
+            expands to new directions, and a
+          </p>
 
-          <div className="flex flex-col ml-12 mb-6">
+          <div className="flex flex-col mt-9">
             <h1 className="text-white lg:text-4xl text-xl font-normal font-artega">
               SAMAHAN that is at the get-go.
             </h1>
