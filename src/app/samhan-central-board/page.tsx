4b04f559dcc593910c337f65813e27411bd7bf50
iyabc <juliennepanes@gmail.com>
2023-07-18 23:47:53 +0800

    feat(SCB): added content to SCBClickableCardsSection, change px of scb page

--- src/app/samhan-central-board/page.tsx
+++ src/app/samhan-central-board/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import SCBClickableCardsSection from '@/components/SCB/SCBClickableCardsSection';
 import SCBTitleSection from '@/components/SCB/SCBTitleSection';
 
 const page = () => {
   return (
-    <div className="px-4 ">
+    <div className="px-8">
       <SCBTitleSection />
+      <SCBClickableCardsSection />
     </div>
   );
 };
--- src/components/SCB/SCBClickableCardsSection.tsx
+++ src/components/SCB/SCBClickableCardsSection.tsx
@@ -1,61 +1,69 @@
 import React from 'react';
-// import Link from 'next/link';
+import Link from 'next/link';
 
-// import type { largeOfficer } from 'lib/types/LargeOfficerType';
-// import CunananImage from '/public/assets/images/Cunanan.jpg';
-// import DabonImage from '/public/assets/images/Dabon.jpg';
-// import GomezImage from '/public/assets/images/Gomez.jpg';
-// import LegaspinaImage from '/public/assets/images/Legaspina.jpg';
-// import OfficerLargeCard from '@/components/ui/OfficerLargeCard';
+import type { largeOfficer } from 'lib/types/largeOfficerType';
 
-// const SCBOfficers: largeOfficer[] = [
-//   {
-//     officer: {
-//       firstName: 'Chyna Marie',
-//       lastName: 'Legaspina',
-//       position: 'President',
-//     },
-//     imageUrl: LegaspinaImage,
-//     link: 'OSP',
-//   },
-//   {
-//     officer: {
-//       firstName: 'Daniel Dave',
-//       lastName: 'Gomez',
-//       position: 'Vice President',
-//     },
-//     imageUrl: GomezImage,
-//     link: 'OVP',
-//   },
-//   {
-//     officer: {
-//       firstName: 'Zoreyn Victorio',
-//       lastName: 'Dabon',
-//       position: 'Secretary-General',
-//     },
-//     imageUrl: DabonImage,
-//     link: 'OSG',
-//   },
-//   {
-//     officer: {
-//       firstName: 'Michelle Anne',
-//       lastName: 'Cunanan',
-//       position: 'Treasurer',
-//     },
-//     imageUrl: CunananImage,
-//     link: 'OST',
-//   },
-// ];
+import CunananImage from '/public/assets/images/Cunanan.jpg';
+import DabonImage from '/public/assets/images/Dabon.jpg';
+import GomezImage from '/public/assets/images/Gomez.jpg';
+import LegaspinaImage from '/public/assets/images/Legaspina.jpg';
+import OfficerLargeCard from '@/components/ui/OfficerLargeCard';
+import { cn } from '@/utils/cvaUtils';
+
+const SCBOfficers: largeOfficer[] = [
+  {
+    officer: {
+      firstName: 'Chyna Marie',
+      lastName: 'Legaspina',
+      position: 'President',
+    },
+    imageUrl: LegaspinaImage,
+    link: 'office-of-the-president',
+  },
+  {
+    officer: {
+      firstName: 'Daniel Dave',
+      lastName: 'Gomez',
+      position: 'Vice President',
+    },
+    imageUrl: GomezImage,
+    link: 'office-of-the-vice-president',
+  },
+  {
+    officer: {
+      firstName: 'Zoreyn Victorio',
+      lastName: 'Dabon',
+      position: 'Secretary-General',
+    },
+    imageUrl: DabonImage,
+    link: 'office-of-the-secretary-general',
+  },
+  {
+    officer: {
+      firstName: 'Michelle Anne',
+      lastName: 'Cunanan',
+      position: 'Treasurer',
+    },
+    imageUrl: CunananImage,
+    link: 'office-of-the-treasurer',
+  },
+];
 
 const SCBClickableCardsSection = () => {
   return (
-    <section className="bg-lightBlue mt-5 rounded-section-card py-12 px-5 md:py-12 lg:py-16">
+    <section className="bg-lightBlue mt-5 rounded-section-card py-12 px-5 md:py-12 lg:py-16 lg:px-7">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
-        SCB Clickable Section
-        {/* {SCBOfficers.map((officer: largeOfficer, index: number) => {
+        {SCBOfficers.map((officer: largeOfficer, index: number) => {
           if (officer.link) {
             return (
-              <Link href={officer.link} key={index} className="">
+              <Link
+                href={officer.link}
+                key={index}
+                className={cn(
+                  'flex sm:justify-center',
+                  index % 2 === 0 ? 'lg:justify-end' : 'lg:justify-start'
+                )}
+              >
                 <OfficerLargeCard
                   officer={{
                     firstName: officer.officer.firstName,
@@ -68,7 +76,7 @@ const SCBClickableCardsSection = () => {
               </Link>
             );
           }
-        })} */}
+        })}
       </div>
     </section>
   );
