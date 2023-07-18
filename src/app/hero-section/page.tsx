488d5f53eac6b3fbf1955e163525f3137ae8c433
louispawaon <lpawaon@gmail.com>
2023-07-18 18:35:09 +0800

    feat: hero section and story, initial commit

---
+++ src/app/hero-section/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import HeroSection from "@/components/Home/HeroSection";
+
+const page = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/Home/HeroSection.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import AtTheGetGoDefinition from "@/components/ui/Hero/AtTheGetGoDefinition";
+import SeeBelow from "@/components/ui/Hero/SeeBelow";
+
+const HeroSection: React.FC = () => {
+  return (
+    <div>
+      <AtTheGetGoDefinition />
+      <SeeBelow />
+    </div>
+  );
+};
+
+export default HeroSection;
