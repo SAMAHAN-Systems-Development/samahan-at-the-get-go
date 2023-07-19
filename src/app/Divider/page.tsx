466d6c412a504b25e6a460c272816e0c572dc0e4
Migs <miguelemmanueldailisan@gmail.com>
2023-07-19 16:08:38 +0800

    feat: added component and story

--- src/app/Divider/page.tsx
+++ src/app/Divider/page.tsx
@@ -3,7 +3,10 @@ import Divider from '@/components/ui/Divider';
 export default function Home() {
   return (
     <main>
-      <Divider text="The Samahan ng mga Mag-aaral ng Pamantasang ng Ateneo de Davao, or SAMAHAN, is the lone autonomous Student Government of the College Unit of Ateneo de Davao University." />
+      <Divider
+        color="default"
+        text="The Samahan ng mga Mag-aaral ng Pamantasang ng Ateneo de Davao, or SAMAHAN, is the lone autonomous Student Government of the College Unit of Ateneo de Davao University."
+      />
     </main>
   );
 }
--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
+import type { VariantProps } from 'class-variance-authority';
+import { cva } from 'class-variance-authority';
+
+const dividerVariants = cva(
+  ['w-full h-8 overflow-hidden rounded-md relative'],
+  {
+    variants: {
+      color: {
+        default: 'bg-orange',
+        blue: 'bg-blue',
+      },
+    },
+  }
+);
+
 type TextProps = {
   text: string;
-};
+  color: string;
+} & VariantProps<typeof dividerVariants>;
 
-const Divider = (props: TextProps) => {
+const Divider: React.FC<TextProps> = ({ text, color }) => {
   return (
-    <div className="w-full overflow-hidden bg-orange rounded-md relative">
-      <p className="text-white inline-block absolute">{props.text}</p>
+    <div className={dividerVariants({ color })}>
+      <p className="text-white inline-block absolute">{text}</p>
     </div>
   );
 };
