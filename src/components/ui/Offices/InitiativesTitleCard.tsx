43afcdd063ceec7782c5c715cd0d6ec639df943a
louispawaon <lpawaon@gmail.com>
2023-07-19 14:12:19 +0800

    refactor: applied changes requested

--- src/components/ui/Offices/InitiativesTitleCard.tsx
+++ src/components/ui/Offices/InitiativesTitleCard.tsx
@@ -2,20 +2,20 @@ import React from "react";
 
 const InitiativesTitleCard: React.FC = () => {
   return (
-    <div className="h-[502px] w-auto md:h-[772px] lg:h-[562px] bg-orange m-1 p-4 md:px-8 md:py-[60px] lg:px-[60px] lg:py-8 flex flex-col justify-between rounded-section-card text-[#FFFF]">
+    <div className="h-[31.375rem] w-auto md:h-[48.25rem] lg:h-[35.125rem] bg-orange m-1 p-4 md:px-8 md:py-[3.75rem] lg:px-[3.75rem] lg:py-8 flex flex-col justify-between rounded-section-card text-white">
       <div className="flex justify-between">
-        <h2 className="flex font-artega text-[20px] md:text-[20px] lg:text-[30px] font-[500]">
+        <h2 className="flex font-artega text-[1.25rem] md:text-[1.25rem] lg:text-[1.875rem] font-medium">
           04
         </h2>
-        <h2 className="flex font-appleGaramond italic font-[700] text-[20px] md:text-[20px] lg:text-[30px]">
+        <h2 className="flex font-appleGaramond italic font-bold text-[1.25rem] md:text-[1.25rem] lg:text-[1.875rem]">
           #AtTheGetGo
         </h2>
       </div>
       <div className="flex justify-between items-end">
-        <h2 className="flex font-artega text-[35px] md:text-[60px] lg:text-[75px] font-[500]">
+        <h2 className="flex font-artega text-[2.188rem] md:text-[3.75rem] lg:text-[4.688rem] font-medium">
           Initiatives
         </h2>
-        <h2 className="flex text-[10px] md:text-[23px] lg:text-[25px] font-[400] p-2">
+        <h2 className="flex text-[0.625rem] md:text-[1.438rem] lg:text-[1.563rem] font-normal p-2">
           SAMAHAN
         </h2>
       </div>
