823198d3f8359e6ea155ba32c20135670bc6a334
Migs <miguelemmanueldailisan@gmail.com>
2023-07-24 16:13:12 +0800

    refactor: applied changes

--- src/stories/components/ui/About/AbouThirdSectionOrangeCard.stories.tsx
+++ src/stories/components/ui/About/AbouThirdSectionOrangeCard.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import AboutThirdSectionOrangeCard from '@/components/ui/AboutPage/AboutThirdSection/WeNavigateCard';
 
 const meta: Meta<typeof AboutThirdSectionOrangeCard> = {
-  title: 'About/About Third Section Orange Card',
+  title: 'About/We Navigate Card',
   component: AboutThirdSectionOrangeCard,
 };
 
