c9d5cd96033437c06762155317b4da48a91e8972
louispawaon <lpawaon@gmail.com>
2023-07-26 09:49:15 +0800

    fix: applied changes to department section

--- src/components/ui/Offices/DepartmentSection.tsx
+++ src/components/ui/Offices/DepartmentSection.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const DepartmentSection = () => {
-  return <div>DepartmentSection</div>;
+import OfficerSmallCard from '@/components/ui/OfficerSmallCard';
+import { Department } from '@/data/Departments';
+
+const DepartmentSection: React.FC = () => {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5 md:gap-x-5">
+        {Department.map((item, index) => (
+          <OfficerSmallCard key={index} {...item} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default DepartmentSection;
