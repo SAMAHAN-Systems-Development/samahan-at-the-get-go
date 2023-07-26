9f1b4882d193b53615c2e775212c168fb88f6038
louispawaon <lpawaon@gmail.com>
2023-07-26 15:25:44 +0800

    fix: applied styles for section

--- src/components/ui/SCB/RepresentativesSection.tsx
+++ src/components/ui/SCB/RepresentativesSection.tsx
@@ -6,7 +6,7 @@ import { Representatives } from '@/data/Representatives';
 const RepresentativesSection: React.FC = () => {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5 md:gap-x-5">
+      <div className="border-solid border-card border-lightBlue rounded-section-card py-20 m-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {Representatives.map((item, index) => (
           <OfficerSmallCard key={index} {...item} />
         ))}
