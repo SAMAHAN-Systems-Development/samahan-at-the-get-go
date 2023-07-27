9a8a005cc645e82c55d1e1fa7fbd1bbc0bd99cc8
louispawaon <lpawaon@gmail.com>
2023-07-27 14:13:24 +0800

    refactor: adjusted representatives to fit to officerType, applied suggested changes

--- src/components/ui/SCB/RepresentativesSection.tsx
+++ src/components/ui/SCB/RepresentativesSection.tsx
@@ -2,34 +2,27 @@ import React from 'react';
 
 import OfficerSmallCard from '@/components/ui/OfficerSmallCard';
 import { Representatives } from '@/data/Representatives';
+import { cn } from '@/utils/cvaUtils';
 
 const RepresentativesSection: React.FC = () => {
   const isLastCardCentered = Representatives.length % 2 === 1;
 
   return (
     <>
-      <div
-        className={`border-solid border-card border-lightBlue rounded-section-card py-20 m-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3`}
-      >
+      <div className="border-solid border-card border-lightBlue rounded-section-card py-20 m-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {Representatives.map((item, index) => (
           <div
             key={index}
-            className={`flex flex-col items-center ${
+            className={cn(
+              'flex flex-col items-center',
               isLastCardCentered && index === Representatives.length - 1
-                ? 'md:col-span-2 lg:hidden'
+                ? 'md:col-span-2 lg:col-auto lg:row-auto lg:justify-self-center'
                 : ''
-            }`}
+            )}
           >
             <OfficerSmallCard {...item} />
           </div>
         ))}
-        {isLastCardCentered && (
-          <div className="hidden lg:flex lg:items-center lg:justify-center">
-            <OfficerSmallCard
-              {...Representatives[Representatives.length - 1]}
-            />
-          </div>
-        )}
       </div>
     </>
   );
--- src/data/Representatives.ts
+++ src/data/Representatives.ts
@@ -1,4 +1,4 @@
-import type { officer } from 'lib/types/officerType';
+import type { OfficerType } from 'lib/types/officerType';
 import Canonigo from 'public/assets/images/Canonigo.jpg';
 import Cortes from 'public/assets/images/Cortes.jpg';
 import Lapatis from 'public/assets/images/Lapatis.jpg';
@@ -9,77 +9,59 @@ import Reyes from 'public/assets/images/Reyes.jpg';
 import Santella from 'public/assets/images/Santella.jpg';
 import Tulipas from 'public/assets/images/Tulipas.jpg';
 
-export const Representatives: officer[] = [
+export const Representatives: OfficerType[] = [
   {
-    officer: {
-      position: 'Accountancy Cluster Representative',
-      firstName: 'Vincent Cesar',
-      lastName: 'Canonigo',
-    },
+    position: 'Accountancy Cluster Representative',
+    firstName: 'Vincent Cesar',
+    lastName: 'Canonigo',
     imageUrl: Canonigo,
   },
   {
-    officer: {
-      position: 'Business and Management Representative',
-      firstName: 'Justin Matthew',
-      lastName: 'Nazareno',
-    },
+    position: 'Business and Management Representative',
+    firstName: 'Justin Matthew',
+    lastName: 'Nazareno',
     imageUrl: Nazareno,
   },
   {
-    officer: {
-      position: 'Computer Studies Representative',
-      firstName: 'Ariane Mae',
-      lastName: 'Mendez',
-    },
+    position: 'Computer Studies Representative',
+    firstName: 'Ariane Mae',
+    lastName: 'Mendez',
     imageUrl: Mendez,
   },
   {
-    officer: {
-      position: 'Hummanities and Lettters Representative',
-      firstName: 'Donnabel',
-      lastName: 'Nobleza',
-    },
+    position: 'Hummanities and Lettters Representative',
+    firstName: 'Donnabel',
+    lastName: 'Nobleza',
     imageUrl: Nobleza,
   },
   {
-    officer: {
-      position: 'Natural Science and Mathematics Representative',
-      firstName: 'Carrie Louie',
-      lastName: 'Tulipas',
-    },
+    position: 'Natural Science and Mathematics Representative',
+    firstName: 'Carrie Louie',
+    lastName: 'Tulipas',
     imageUrl: Tulipas,
   },
   {
-    officer: {
-      position: 'School of Engineering and Architecture Representative',
-      firstName: 'Cai Roxana',
-      lastName: 'Reyes',
-    },
+    position: 'School of Engineering and Architecture Representative',
+    firstName: 'Cai Roxana',
+    lastName: 'Reyes',
     imageUrl: Reyes,
   },
   {
-    officer: {
-      position: 'School of Education Representative',
-      firstName: 'Monica Gwen',
-      lastName: 'Cortes',
-    },
+    position: 'School of Education Representative',
+    firstName: 'Monica Gwen',
+    lastName: 'Cortes',
     imageUrl: Cortes,
   },
   {
-    officer: {
-      position: 'School of Nursing Representative',
-      firstName: 'Leen Juliet',
-      lastName: 'Lapatis',
-    },
+    position: 'School of Nursing Representative',
+    firstName: 'Leen Juliet',
+    lastName: 'Lapatis',
     imageUrl: Lapatis,
   },
   {
-    officer: {
-      position: 'Social Sciences Representative',
-      firstName: 'Rey Angelo',
-      lastName: 'Santella',
-    },
+    position: 'Social Sciences Representative',
+    firstName: 'Rey Angelo',
+    lastName: 'Santella',
     imageUrl: Santella,
   },
 ];
