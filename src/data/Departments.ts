cffd9a55f35af7746df6f8055316eeb0f83337dc
louispawaon <lpawaon@gmail.com>
2023-07-27 20:43:17 +0800

    refactor: added images for department directors and deputy directors (incomplete)

--- src/data/Departments.ts
+++ src/data/Departments.ts
@@ -1,230 +1,203 @@
-import type { officer } from 'lib/types/officerType';
+import type { OfficerType } from 'lib/types/officerType';
+import Alkuino from 'public/assets/images/Alkuino.jpg';
+import Amarga from 'public/assets/images/Amarga.jpg';
+import Ancog from 'public/assets/images/Ancog.jpg';
+import Bacusmo from 'public/assets/images/Bacusmo.jpg';
+import Bitana from 'public/assets/images/Bitana.jpg';
+import Cagmat from 'public/assets/images/Cagmat.jpg';
+import Cajes from 'public/assets/images/Cajes.jpg';
 import placeholder from 'public/assets/images/Canonigo.jpg';
+import Cortez from 'public/assets/images/Cortez.jpg';
+import Estrada from 'public/assets/images/Estrada.jpg';
+import Gempesaw from 'public/assets/images/Gempesaw.jpg';
+import Hernandez from 'public/assets/images/Hernandez.jpg';
+import Hinojales from 'public/assets/images/Hinojales.jpg';
+import Lapating from 'public/assets/images/Lapating.jpg';
+import Lumain from 'public/assets/images/Lumain.jpg';
+import Manalo from 'public/assets/images/Manalo.jpg';
+import Medida from 'public/assets/images/Medida.jpg';
+import Pahinado from 'public/assets/images/Pahinado.jpg';
+import Pawaon from 'public/assets/images/Pawaon.jpg';
+import Rosal from 'public/assets/images/Rosal.jpg';
+import Saure from 'public/assets/images/Saure.jpg';
+import Sy from 'public/assets/images/Sy.jpg';
 
-export const OSPDepartment: officer[] = [
+export const OSPDepartment: OfficerType[] = [
   {
-    officer: {
-      position: 'Director',
-      department: 'Department of External Affairs',
-      firstName: 'Denise Maxyn',
-      lastName: 'Amarga',
-    },
+    position: 'Director',
+    department: 'Department of External Affairs',
+    firstName: 'Denise Maxyn',
+    lastName: 'Amarga',
+    imageUrl: Amarga,
+  },
+  {
+    position: 'Deputy Director',
+    department: 'Department of External Affairs',
+    firstName: 'Chloe Mikaela',
+    lastName: 'Gempesaw',
+    imageUrl: Gempesaw,
+  },
+  {
+    position: 'Director',
+    department: 'Department of Campaigns & Advocacy',
+    firstName: 'Juven',
+    lastName: 'Bacusmo',
+    imageUrl: Bacusmo,
+  },
+  {
+    position: 'Deputy Director',
+    department: 'Department of Campaigns & Advocacy',
+    firstName: 'Algene Derek Louis',
+    lastName: 'Lago',
     imageUrl: placeholder,
   },
   {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Department of External Affairs',
-      firstName: 'Chloe Mikaela',
-      lastName: 'Gempesaw',
-    },
+    position: 'Director',
+    department: 'Ecoteneo Student Unit',
+    firstName: 'Mary Jastine',
+    lastName: 'Lapating',
+    imageUrl: Lapating,
+  },
+  {
+    position: 'Deputy Director',
+    department: 'Ecoteneo Student Unit',
+    firstName: 'Joegold Raphaelle',
+    lastName: 'Cajes',
+    imageUrl: Cajes,
+  },
+];
+
+export const OVPDepartment: OfficerType[] = [
+  {
+    position: 'Director',
+    department: 'Department of Academic Affairs',
+    firstName: 'Aliya',
+    lastName: 'Medida',
+    imageUrl: Medida,
+  },
+  {
+    position: 'Deputy Director',
+    department: 'Department of Academic Affairs',
+    firstName: 'Ralph Rae',
+    lastName: 'Pahinado',
+    imageUrl: Pahinado,
+  },
+  {
+    position: 'Director',
+    department: 'Department of Disaster Risk Reduction & Management',
+    firstName: 'Justine Nicole',
+    lastName: 'Buccat',
     imageUrl: placeholder,
   },
   {
-    officer: {
-      position: 'Director',
-      department: 'Department of Campaigns & Advocacy',
-      firstName: 'Juven',
-      lastName: 'Bacusmo',
-    },
-    imageUrl: placeholder,
-  },
-  {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Department of Campaigns & Advocacy',
-      firstName: 'Algene Derek Louis',
-      lastName: 'Lago',
-    },
-    imageUrl: placeholder,
-  },
-  {
-    officer: {
-      position: 'Director',
-      department: 'Ecoteneo Student Unit',
-      firstName: 'Mary Jastine',
-      lastName: 'Lapating',
-    },
-    imageUrl: placeholder,
-  },
-  {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Ecoteneo Student Unit',
-      firstName: 'Joegold Raphaelle',
-      lastName: 'Cajes',
-    },
+    position: 'Deputy Director',
+    department: 'Department of Disaster Risk Reduction & Management',
+    firstName: 'Joshua Emil',
+    lastName: 'Narisma',
     imageUrl: placeholder,
   },
 ];
 
-export const OVPDepartment: officer[] = [
+export const OSGDepartment: OfficerType[] = [
   {
-    officer: {
-      position: 'Director',
-      department: 'Department of Academic Affairs',
-      firstName: 'Aliya',
-      lastName: 'Medida',
-    },
-    imageUrl: placeholder,
+    position: 'Director',
+    department: 'Samahan Creative Team',
+    firstName: 'Maria Caryl',
+    lastName: 'Manalo',
+    imageUrl: Manalo,
   },
   {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Department of Academic Affairs',
-      firstName: 'Ralph Rae',
-      lastName: 'Pahinado',
-    },
-    imageUrl: placeholder,
+    position: 'Deputy Director',
+    department: 'Samahan Creative Team',
+    firstName: 'Miguel Antonio',
+    lastName: 'Estrada',
+    imageUrl: Estrada,
   },
   {
-    officer: {
-      position: 'Director',
-      department: 'Department of Disaster Risk Reduction & Management',
-      firstName: 'Justine Nicole',
-      lastName: 'Buccat',
-    },
-    imageUrl: placeholder,
+    position: 'Director',
+    department: 'Ateneo Samahan Productions',
+    firstName: 'Hannah Nicole',
+    lastName: 'Bitana',
+    imageUrl: Bitana,
   },
   {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Department of Disaster Risk Reduction & Management',
-      firstName: 'Joshua Emil',
-      lastName: 'Narisma',
-    },
-    imageUrl: placeholder,
-  },
-];
-
-export const OSGDepartment: officer[] = [
-  {
-    officer: {
-      position: 'Director',
-      department: 'Samahan Creative Team',
-      firstName: 'Maria Caryl',
-      lastName: 'Manalo',
-    },
-    imageUrl: placeholder,
+    position: 'Deputy Director',
+    department: 'Ateneo Samahan Productions',
+    firstName: 'Ken Ryle',
+    lastName: 'Hinojales',
+    imageUrl: Hinojales,
   },
   {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Samahan Creative Team',
-      firstName: 'Miguel Antonio',
-      lastName: 'Estrada',
-    },
-    imageUrl: placeholder,
+    position: 'Director',
+    department: 'Samahan Communications Department',
+    firstName: 'Julianne Kaye',
+    lastName: 'Cortez',
+    imageUrl: Cortez,
   },
   {
-    officer: {
-      position: 'Director',
-      department: 'Ateneo Samahan Productions',
-      firstName: 'Hannah Nicole',
-      lastName: 'Bitana',
-    },
-    imageUrl: placeholder,
+    position: 'Deputy Director',
+    department: 'Samahan Communications Department',
+    firstName: 'Andy Judd',
+    lastName: 'Lumain',
+    imageUrl: Lumain,
   },
   {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Ateneo Samahan Productions',
-      firstName: 'Ken Ryle',
-      lastName: 'Hinojales',
-    },
-    imageUrl: placeholder,
+    position: 'Director',
+    department: 'Department of Research & Development',
+    firstName: 'Jerachris Megaela',
+    lastName: 'Rosal',
+    imageUrl: Rosal,
   },
   {
-    officer: {
-      position: 'Director',
-      department: 'Samahan Communications Department',
-      firstName: 'Julianne Kaye',
-      lastName: 'Cortez',
-    },
-    imageUrl: placeholder,
+    position: 'Deputy Director',
+    department: 'Department of Research & Development',
+    firstName: 'Tweeky',
+    lastName: 'Saure',
+    imageUrl: Saure,
   },
   {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Samahan Communications Department',
-      firstName: 'Andy Judd',
-      lastName: 'Lumain',
-    },
-    imageUrl: placeholder,
+    position: 'Director',
+    department: 'Department of Systems Development',
+    firstName: 'Louis Miguel',
+    lastName: 'Pawaon',
+    imageUrl: Pawaon,
   },
   {
-    officer: {
-      position: 'Director',
-      department: 'Department of Research & Development',
-      firstName: 'Jerachris Megaela',
-      lastName: 'Rosal',
-    },
-    imageUrl: placeholder,
-  },
-  {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Department of Research & Development',
-      firstName: 'Tweeky',
-      lastName: 'Saure',
-    },
-    imageUrl: placeholder,
-  },
-  {
-    officer: {
-      position: 'Director',
-      department: 'Department of Systems Development',
-      firstName: 'Louis Miguel',
-      lastName: 'Pawaon',
-    },
-    imageUrl: placeholder,
-  },
-  {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Department of Systems Development',
-      firstName: 'Angelli',
-      lastName: 'Alkuino',
-    },
-    imageUrl: placeholder,
+    position: 'Deputy Director',
+    department: 'Department of Systems Development',
+    firstName: 'Angelli',
+    lastName: 'Alkuino',
+    imageUrl: Alkuino,
   },
 ];
 
-export const OSTDepartment: officer[] = [
+export const OSTDepartment: OfficerType[] = [
   {
-    officer: {
-      position: 'Director',
-      department: 'Samahan Logistics Department',
-      firstName: 'Kristine Michaella',
-      lastName: 'Cagmat',
-    },
-    imageUrl: placeholder,
+    position: 'Director',
+    department: 'Samahan Logistics Department',
+    firstName: 'Kristine Michaella',
+    lastName: 'Cagmat',
+    imageUrl: Cagmat,
   },
   {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Samahan Logistics Department',
-      firstName: 'Emmanuelle Adrian',
-      lastName: 'Ancog',
-    },
-    imageUrl: placeholder,
+    position: 'Deputy Director',
+    department: 'Samahan Logistics Department',
+    firstName: 'Emmanuelle Adrian',
+    lastName: 'Ancog',
+    imageUrl: Ancog,
   },
   {
-    officer: {
-      position: 'Director',
-      department: 'Department of Sponsorships & Support',
-      firstName: 'Raphael Ray',
-      lastName: 'Hernandez',
-    },
-    imageUrl: placeholder,
+    position: 'Director',
+    department: 'Department of Sponsorships & Support',
+    firstName: 'Raphael Ray',
+    lastName: 'Hernandez',
+    imageUrl: Hernandez,
   },
   {
-    officer: {
-      position: 'Deputy Director',
-      department: 'Department of Sponsorships & Support',
-      firstName: 'Maxine Christiana',
-      lastName: 'Sy',
-    },
-    imageUrl: placeholder,
+    position: 'Deputy Director',
+    department: 'Department of Sponsorships & Support',
+    firstName: 'Maxine Christiana',
+    lastName: 'Sy',
+    imageUrl: Sy,
   },
 ];
