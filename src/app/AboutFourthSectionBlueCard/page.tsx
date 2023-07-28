a787cff6730da564bdf85ceead8241df644db29e
Migs <miguelemmanueldailisan@gmail.com>
2023-07-28 23:45:32 +0800

    chore: removed testing page from app dir

--- src/app/AboutFourthSectionBlueCard/page.tsx
+++
@@ -1,9 +0,0 @@
-import AboutForthSectionBlueCard from '@/components/ui/AboutPage/AboutFourthSection/ThisYearSamahanCard';
-
-export default function Home() {
-  return (
-    <main>
-      <AboutForthSectionBlueCard />
-    </main>
-  );
-}
