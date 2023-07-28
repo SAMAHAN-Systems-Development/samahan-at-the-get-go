38f7c3e24fb042b83283f4445640c8aff996bd56
Migs <miguelemmanueldailisan@gmail.com>
2023-07-29 00:04:59 +0800

    chore: removed testing page from app dir

--- src/app/AboutFourthSectionOrangeCard/page.tsx
+++
@@ -1,9 +0,0 @@
-import AboutForthSectionOrangeCard from '@/components/ui/AboutPage/AboutFourthSection/AtTheGetGoCard';
-
-export default function Home() {
-  return (
-    <main>
-      <AboutForthSectionOrangeCard />
-    </main>
-  );
-}
