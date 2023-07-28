b06f9e05d7478f9d5a42219535b8d3c3ce2c19da
louispawaon <lpawaon@gmail.com>
2023-07-28 11:48:23 +0800

    refactor: set custom grid config for every office

--- src/components/ui/Offices/DepartmentSection.tsx
+++ src/components/ui/Offices/DepartmentSection.tsx
@@ -7,10 +7,9 @@ import {
   OSTDepartment,
   OVPDepartment,
 } from '@/data/Departments';
-import { cn } from '@/utils/cvaUtils';
 
 type OfficeDepartmentSectionProps = {
-  office: string;
+  office: 'OSP' | 'OVP' | 'OSG' | 'OST';
 };
 
 const DepartmentSection: React.FC<OfficeDepartmentSectionProps> = ({
@@ -31,35 +30,39 @@ const DepartmentSection: React.FC<OfficeDepartmentSectionProps> = ({
     }
   };
 
-  const isLastCardCentered = office.length % 2 === 1;
+  const departmentData = handleData(office);
+
+  const departmentGridColumns: {
+    [key in OfficeDepartmentSectionProps['office']]: number;
+  } = {
+    OSP: 3,
+    OVP: 4,
+    OSG: 2,
+    OST: 4,
+  };
+
+  const gridColumns = departmentGridColumns[office];
+
   return (
     <section className="container border-card border-lightBlue mt-5 rounded-section-card py-12 px-5 md:py-12 lg:py-16 lg:px-7">
       <div className="flex justify-between text-xl text-lightBlue mb-11 px-4">
         <p className="font-artega">03</p>
         <p className="uppercase">Departments</p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {handleData(office).map((officer, index) => {
-          return (
-            <div
-              key={index}
-              className={cn(
-                'flex flex-col items-center',
-                isLastCardCentered && index === office.length - 1
-                  ? 'md:col-span-2 lg:col-auto lg:row-auto lg:justify-self-center'
-                  : ''
-              )}
-            >
-              <OfficerSmallCard
-                firstName={officer.firstName}
-                lastName={officer.lastName}
-                position={officer.position}
-                department={officer.department}
-                imageUrl={officer.imageUrl}
-              />
-            </div>
-          );
-        })}
+      <div
+        className={`gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-${gridColumns}`}
+      >
+        {departmentData.map((officer, index) => (
+          <div key={index} className="flex flex-col items-center">
+            <OfficerSmallCard
+              firstName={officer.firstName}
+              lastName={officer.lastName}
+              position={officer.position}
+              department={officer.department}
+              imageUrl={officer.imageUrl}
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
