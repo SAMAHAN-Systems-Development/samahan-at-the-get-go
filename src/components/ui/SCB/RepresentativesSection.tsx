8e13a9e4a622e0c53ea3851b7254f85f186e7853
louispawaon <lpawaon@gmail.com>
2023-07-28 10:02:46 +0800

    refactor: adjusted gap for each representative component

--- src/components/ui/SCB/RepresentativesSection.tsx
+++ src/components/ui/SCB/RepresentativesSection.tsx
@@ -8,23 +8,21 @@ const RepresentativesSection: React.FC = () => {
   const isLastCardCentered = Representatives.length % 2 === 1;
 
   return (
-    <>
-      <div className="border-solid border-card border-lightBlue rounded-section-card py-20 m-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {Representatives.map((item, index) => (
-          <div
-            key={index}
-            className={cn(
-              'flex flex-col items-center',
-              isLastCardCentered && index === Representatives.length - 1
-                ? 'md:col-span-2 lg:col-auto lg:row-auto lg:justify-self-center'
-                : ''
-            )}
-          >
-            <OfficerSmallCard {...item} />
-          </div>
-        ))}
-      </div>
-    </>
+    <section className="border-solid border-card border-lightBlue rounded-section-card py-20 m-2 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {Representatives.map((item, index) => (
+        <div
+          key={index}
+          className={cn(
+            'flex flex-col items-center',
+            isLastCardCentered && index === Representatives.length - 1
+              ? 'md:col-span-2 lg:col-auto lg:row-auto lg:justify-self-center'
+              : ''
+          )}
+        >
+          <OfficerSmallCard {...item} />
+        </div>
+      ))}
+    </section>
   );
 };
 
