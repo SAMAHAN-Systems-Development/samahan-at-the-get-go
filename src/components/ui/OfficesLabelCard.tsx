4393e3736ab09d6269889c509216edb141df4f98
iyabc <juliennepanes@gmail.com>
2023-07-28 20:59:50 +0800

    feat(OfficesLabelCard): added breakpoint prefix for title and arrow image (responsive)

--- src/components/ui/OfficesLabelCard.tsx
+++ src/components/ui/OfficesLabelCard.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 type OfficesLabelCardProps = {
   title: string;
@@ -7,11 +7,11 @@ type OfficesLabelCardProps = {
 
 const OfficesLabelCard: React.FC<OfficesLabelCardProps> = ({ title }) => {
   return (
-    <div className="flex justify-between items-center rounded-section-card bg-lightBlue px-4 lg:px-14 py-3">
-      <p className="font-artea font-semibold uppercase text-white lg:text-[1.6rem] ">
+    <div className="flex justify-between items-center rounded-section-card bg-lightBlue px-5 lg:px-14 py-4">
+      <p className="font-artega font-semibold uppercase text-white text-xs lg:leading-none sm:text-base lg:text-2xl ">
         {title}
       </p>
-      <div className="relative w-16 h-10">
+      <div className="relative h-10 w-16 sm:w-32 lg:w-40 ">
         <Image
           src="/assets/images/orange_arrow.png"
           alt="Orange Arrow"
