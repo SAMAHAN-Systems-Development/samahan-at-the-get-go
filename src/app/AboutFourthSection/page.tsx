90b68b4144e0de0ddffc7914e6bb1a108a7dbb6b
Migs <miguelemmanueldailisan@gmail.com>
2023-07-30 00:05:43 +0800

    chore: deleted testing page in app dir

--- src/app/AboutFourthSection/page.tsx
+++
@@ -1,9 +0,0 @@
-import AboutFourthSection from "@/components/ui/AboutPage/AboutFourthSection/AboutFourthSection";
-
-export default function Home() {
-  return (
-    <main>
-      <AboutFourthSection />
-    </main>
-  );
-}
