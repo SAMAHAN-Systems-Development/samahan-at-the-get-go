b31c4a8047419a2855acb501c45799d6fa82855b
Migs <miguelemmanueldailisan@gmail.com>
2023-07-29 16:54:43 +0800

    chore: deleted old story || modified imports

--- src/components/ui/AboutPage/AboutFourthSection/AboutFourthSection.tsx
+++ src/components/ui/AboutPage/AboutFourthSection/AboutFourthSection.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import AboutForthSectionBlueCard from '@/components/ui/AboutPage/AboutFourthSection/AboutForthSectionBlueCard';
-import AboutForthSectionOrangeCard from '@/components/ui/AboutPage/AboutFourthSection/AboutForthSectionOrangeCard';
+import AtTheGetGoCard from '@/components/ui/AboutPage/AboutFourthSection/AtTheGetGoCard';
+import ThisYearSamahanCard from '@/components/ui/AboutPage/AboutFourthSection/ThisYearSamahanCard';
 
 const AboutFourthSection = () => {
   return (
     <>
       <div className="relative flex flex-col justify-start w-full gap-2 p-5">
-        <AboutForthSectionBlueCard />
-        <AboutForthSectionOrangeCard />
+        <ThisYearSamahanCard />
+        <AtTheGetGoCard />
       </div>
     </>
   );
