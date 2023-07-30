0b5261bd922e708cedbda671c610dec92c8d267c
Innhahn <jaytan3825@gmail.com>
2023-07-30 21:25:07 +0800

    feat(Divider): added CVA's and added marquee animation on the component

--- src/app/Divider/page.tsx
+++ src/app/Divider/page.tsx
@@ -5,14 +5,12 @@ export default function Home() {
     <main>
       <Divider
         page="aboutPage"
-        style="aboutText"
-        text=" The Samahan ng mga Mag-aaral ng Pamantasang ng Ateneo de Davao, or SAMAHAN, is the lone autonomous Student Government of the College Unit of Ateneo de Davao University. "
+        text="The Samahan ng mga Mag-aaral ng Pamantasang ng Ateneo de Davao, or SAMAHAN, is the lone autonomous Student Government of the College Unit of Ateneo de Davao University.&nbsp;"
       />
       <br />
       <Divider
         page="scbPage"
-        style="scbText"
-        text=" AT THE GET GO AT THE GET GO AT THE GET GO AT THE GET GO AT THE GET GO AT THE GET GO AT THE GET GO AT THE GET GO AT THE GET GO AT THE GET GO AT THE GET GO "
+        text="AT THE GET GO AT THE GET GO AT THE GET GO AT THE GET GO AT THE GET GO&nbsp;"
       />
     </main>
   );
--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -10,34 +10,26 @@ const dividerVariants = cva(
   {
     variants: {
       page: {
-        aboutPage: 'bg-orange py-[1rem] px-4',
-        scbPage: 'bg-blue py-8 px-4 md:py-[2rem] lg:py-[2rem]',
+        aboutPage:
+          'bg-orange py-[1rem] px-4 text-sm sm:text-sm md:text-base lg:text-base italic',
+        scbPage:
+          'bg-blue py-8 px-4 md:py-[2rem] lg:py-[2rem] text-sm font-medium font-artega sm:text-base md:text-base lg:text-[1.625rem]',
       },
     },
   }
 );
-const textVariants = cva(['text-white absolute'], {
-  variants: {
-    style: {
-      aboutText: 'text-xs sm:text-sm md:text-base lg:text-base',
-      scbText:
-        'text-sm font-medium font-artega sm:text-base md:text-base lg:text-[1.625rem]',
-    },
-  },
-});
 
 type TextProps = {
   page: string;
-  style: 'aboutText' | 'scbText';
   text: string;
 } & VariantProps<typeof dividerVariants>;
 
-const Divider: React.FC<TextProps> = ({ page, style, text }) => {
+const Divider: React.FC<TextProps> = ({ page, text }) => {
   return (
-    <div className=" flex overflow-x-hidden">
+    <div className="relative flex overflow-x-hidden">
       <div className={dividerVariants({ page })}>
-        <p className={`${textVariants({ style })} animate-marquee`}>{text}</p>
-        <p className={`${textVariants({ style })} animate-marquee2`}>{text}</p>
+        <p className="text-white absolute animate-marquee">{text}</p>
+        <p className="text-white absolute animate-marquee2">{text}</p>
       </div>
     </div>
   );
