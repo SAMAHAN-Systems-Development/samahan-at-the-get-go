bd4711438143f2f2b1090fa6617c5d798fce334f
Angelli <akmalkuino@addu.edu.ph>
2023-08-08 03:31:09 +0800

    feat: created relevant information area card component

---
+++ src/app/ria-card-demo/page.tsx
@@ -0,0 +1,27 @@
+import RIACard from '@/components/ui/RIACard';
+
+export default function Home() {
+  return (
+    <div className="p-16">
+      <RIACard title="Enrollment">
+        <div className="">
+          Clear all tagged obligations from the previous semester by contacting
+          concerned offices. <br />
+          Choose the schedule of classes of the courses that are required to be
+          taken.
+          <br />
+          Pay the required amount online. Once the payment has been processed,
+          the student will be tagged as “enrolled” in the SIS.
+          <br />
+          Payment will reflect within one to three business days. For other
+          concerns, please message the AdDU Finance on Facebook, or through
+          their telephone number 082 221 2411 loc 8237 or 8217.
+          <br />
+          Print or screenshot the schedule and wait for the start of the
+          semester.
+          <br />
+        </div>
+      </RIACard>
+    </div>
+  );
+}
