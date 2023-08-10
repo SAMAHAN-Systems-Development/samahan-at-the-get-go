75b6d092d8e075423a9884922788b164d6d7fe2f
louispawaon <lpawaon@gmail.com>
2023-08-10 15:02:26 +0800

    refactor(RepresentativesSection): changed bg color, added section title and adjusted grid

--- src/components/ui/SCB/RepresentativesSection.tsx
+++ src/components/ui/SCB/RepresentativesSection.tsx
@@ -8,14 +8,19 @@ const RepresentativesSection: React.FC = () => {
   const isLastCardCentered = Representatives.length % 2 === 1;
 
   return (
-    <section className="border-solid border-card border-lightBlue rounded-section-card py-20 m-2 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <section className="bg-[#1A275F] rounded-section-card py-20 m-2 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <div className="col-span-full text-center mt-20 mb-7">
+        <h2 className="text-white sm:text-xl lg:text-4xl font-artega font-medium uppercase">
+          Cluster Representatives
+        </h2>
+      </div>
       {Representatives.map((item, index) => (
         <div
           key={index}
           className={cn(
             'flex flex-col items-center',
             isLastCardCentered && index === Representatives.length - 1
-              ? 'md:col-span-2 lg:col-auto lg:row-auto lg:justify-self-center'
+              ? 'md:col-span-2 md:justify-center lg:col-span-4 lg:justify-self-center'
               : ''
           )}
         >
