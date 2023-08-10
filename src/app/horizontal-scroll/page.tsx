3268d52c04eeda18060e456952d60d8976698d3d
louispawaon <lpawaon@gmail.com>
2023-08-10 22:35:12 +0800

    feat: add horizontal scroll and story, initial commit

---
+++ src/app/horizontal-scroll/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import InfiniteMarquee from '@/components/ui/InfiniteMarquee';
+
+const page = () => {
+  return (
+    <>
+      <InfiniteMarquee />
+    </>
+  );
+};
+
+export default page;
