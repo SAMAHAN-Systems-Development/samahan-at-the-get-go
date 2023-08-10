65434d0a1af7efca728d726324741f3c3bda7827
louispawaon <lpawaon@gmail.com>
2023-08-10 15:39:47 +0800

    refactor(DepartmentSection): removed border, added section title

--- src/components/ui/Offices/DepartmentSection.tsx
+++ src/components/ui/Offices/DepartmentSection.tsx
@@ -45,10 +45,11 @@ const DepartmentSection: React.FC<OfficeDepartmentSectionProps> = ({
   const gridColumns = departmentGridColumns[office];
 
   return (
-    <section className="container border-card border-lightBlue mt-5 rounded-section-card py-12 px-5 md:py-12 lg:py-16 lg:px-7">
-      <div className="flex justify-between text-xl text-lightBlue mb-11 px-4">
-        <h2 className="font-artega">03</h2>
-        <h2 className="uppercase">Departments</h2>
+    <section className="container mt-5 rounded-section-card py-12 px-5 md:py-12 lg:py-16 lg:px-7">
+      <div className="col-span-full text-center mt-20 mb-7">
+        <h2 className="text-blue sm:text-xl lg:text-4xl font-artega font-medium uppercase">
+          Departments
+        </h2>
       </div>
       <div className={cn(`gap-4 grid ${gridColumns}`)}>
         {departmentData.map((officer, index) => (
@@ -59,6 +60,7 @@ const DepartmentSection: React.FC<OfficeDepartmentSectionProps> = ({
               position={officer.position}
               department={officer.department}
               imageUrl={officer.imageUrl}
+              textColorClass="text-blue"
             />
           </div>
         ))}
