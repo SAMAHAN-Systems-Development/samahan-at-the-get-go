8c670197229391f60268bed904082282b74b1b7a
louispawaon <lpawaon@gmail.com>
2023-08-13 18:04:32 +0800

    refactor(infiniteMarquee): applied mapping for the at the get go phrase

--- src/components/ui/InfiniteMarquee.tsx
+++ src/components/ui/InfiniteMarquee.tsx
@@ -1,42 +1,33 @@
 import React from 'react';
 
 const InfiniteMarquee = () => {
+  const atTheGetGo = [
+    'At the Get Go',
+    'At the Get Go',
+    'At the Get Go',
+    'At the Get Go',
+    'At the Get Go',
+  ];
+
+  const renderPhrase = () => {
+    return atTheGetGo.map((text, index) => (
+      <span
+        key={index}
+        className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega"
+      >
+        {text}
+      </span>
+    ));
+  };
+
   return (
     <>
       <div className="relative flex overflow-x-hidden text-blue uppercase">
         <div className="py-12 animate-marquee whitespace-nowrap">
-          <span className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega">
-            At the Get Go
-          </span>
-          <span className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega">
-            At the Get Go
-          </span>
-          <span className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega">
-            At the Get Go
-          </span>
-          <span className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega">
-            At the Get Go
-          </span>
-          <span className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega">
-            At the Get Go
-          </span>
+          {renderPhrase()}
         </div>
         <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
-          <span className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega">
-            At the Get Go
-          </span>
-          <span className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega">
-            At the Get Go
-          </span>
-          <span className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega">
-            At the Get Go
-          </span>
-          <span className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega">
-            At the Get Go
-          </span>
-          <span className="mx-4 sm:text-xl md:text-2xl lg:text-4xl font-artega">
-            At the Get Go
-          </span>
+          {renderPhrase()}
         </div>
       </div>
     </>
