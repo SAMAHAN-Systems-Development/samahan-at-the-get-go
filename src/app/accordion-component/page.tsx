d34f2856ff8f131ba42300f3f064f62b7718cb5c
Mariel <marmabs692@gmail.com>
2023-08-13 23:17:22 +0800

    chore: edited and deleted divs

--- src/app/accordion-component/page.tsx
+++ src/app/accordion-component/page.tsx
@@ -1,56 +1,31 @@
 import React from 'react';
 
-import AccordionComponent from '@/components/ui/AccordionComponent/AccordionComponent';
+import AccordionComponent from '@/components/ui/AccordionComponent/AccordionComponentMain';
 
 const page = () => {
   return (
     <div>
       <div className="pt-[1.25rem]">
-        <AccordionComponent question="What is the AdDU Student Information System?">
-          <div>
-            Students are required to check their SIS during pre-registration,
-            enrollment, and the first week of classes for possible issues with
-            their enrollment. Any issue raised belatedly, as resulting from the
-            student’s failure to notice the same in his SIS shall not be
-            corrected thereafter.
+        <AccordionComponent question="What are the Scholarship Programs offered by the School?">
+          <div className="leading-loose">
+            <ol className="list-decimal list-inside">
+              <li>JSF (Exclusive for Valedictorians and Salutatorians)</li>
+              <li>GIA or Grant-in-Aid</li>
+            </ol>
           </div>
         </AccordionComponent>
       </div>
       <div className="pt-[1.25rem]">
-        <AccordionComponent question="What is the student’s obligation in the use of the Student Information System?">
-          <div>
-            Students are required to check their SIS during pre-registration,
-            enrollment, and the first week of classes for possible issues with
-            their enrollment. Any issue raised belatedly, as resulting from the
-            student’s failure to notice the same in his SIS shall not be
-            corrected thereafter.
-          </div>
-        </AccordionComponent>
-      </div>
-      <div className="pt-[1.25rem]">
-        <AccordionComponent question="How do I activate my AdDU?">
+        <AccordionComponent question="How much is/are the grants given to students?">
           <div className=" leading-loose">
             <ol className="list-decimal px-4">
               <li>
-                Take pictures of yourself.
+                JSF
                 <ol className="list-disc list-inside">
-                  <li>
-                    Torso/upper body (includes head down to elbows) holding AdDU
-                    ID close to head
-                  </li>
-                  <li>Close up of face with AdDU ID close to head.</li>
-                  <li>Close up of AdDU ID.</li>
+                  <li>100% Tuition ONLY for valedictorians</li>
+                  <li>50% Tuition ONLY for salutatorians</li>
                 </ol>
               </li>
-              <p>
-                Note: Make sure that the ID and ID number is visible and legible
-                on all three pictures. If AdDU ID is not available, a student
-                may use a{' '}
-                <strong>
-                  driver’s license, passport, voter’s ID, or any proof for
-                  identification.
-                </strong>
-              </p>
               <li>
                 Send an email to gdsabaldan@addu.edu.ph and
                 mtropenia@addu.edu.ph with the attached photos, complete name
@@ -59,45 +34,86 @@ const page = () => {
               <li>Follow the specific instructions provided by them.</li>
             </ol>
             Note: Working hours of the personnel run from 8 AM to 5 PM
-          </div>
+          </div>{' '}
         </AccordionComponent>
       </div>
       <div className="pt-[1.25rem]">
-        <AccordionComponent question="How do I log into my SIS?">
-          <div className=" leading-loose">
-            Your account name is formed from the initials of your first name +
-            middle initial + surname. Here are some examples:
-            <ol className="list-decimal px-4 list-inside">
-              <li>
-                (Theresa Joyce E. Lopez → tjelopez; John Marvin A. De la Cruz →
-                jmdelacruz; M Jessica T. Uy → mjtuy)
-              </li>
-              <li>
-                (If your name has Jr. attached to it, your account name will
-                have the suffix ‘jr’: Victor Emmanuel C. Estuary Jr. →
-                vecestuartjr)
-              </li>
-              <li>
-                (If your name has an ‘ñ’, your account name will have an “n” in
-                its place: Kromyko Justin B. Nuñez → kjbnunez)
-              </li>
-              <li>
-                (If your name has a Roman numeral suffix such as II or III, your
-                account name will have a Roman numeral suffix corresponding to
-                the Roman numeral: Jerry Neil E. Bulao II → jnebulao2)
-              </li>
-            </ol>
-          </div>
-        </AccordionComponent>
-      </div>
-      <div className="pt-[1.25rem]">
-        <AccordionComponent question="What is my SIS username and password?">
+        <AccordionComponent question="How to apply for the Scholarship Program?">
           <div>
-            Your default password is formed from the phrase “AdDU,” followed by
-            your ID barcode. Thus if your ID barcode is 6201706010342Z, your
-            default password is AdDU6201706010342Z. Your SIS username is your
-            GSuite username, and your ADDU email account (@addu.edu.ph). Your
-            GSuite password is the same default password above.
+            Preregistration is the prelisting of students, prior to the
+            enrollment period, of courses they desire to take for the succeeding
+            semester. Students may enlist in classes, depending on slot
+            availability.
+          </div>
+        </AccordionComponent>
+      </div>
+      <div className="pt-[1.25rem]">
+        <AccordionComponent question="I have a Government Grant, is it okay to apply for GIA or JSF Program?">
+          <div>
+            Preregistration is the prelisting of students, prior to the
+            enrollment period, of courses they desire to take for the succeeding
+            semester. Students may enlist in classes, depending on slot
+            availability.
+          </div>
+        </AccordionComponent>
+      </div>
+      <div className="pt-[1.25rem]">
+        <AccordionComponent question="What are the requirements?">
+          <div>
+            Preregistration is the prelisting of students, prior to the
+            enrollment period, of courses they desire to take for the succeeding
+            semester. Students may enlist in classes, depending on slot
+            availability.
+          </div>
+        </AccordionComponent>
+      </div>
+      <div className="pt-[1.25rem]">
+        <AccordionComponent question="Does GIA or JSF have allowances?">
+          <div>
+            Preregistration is the prelisting of students, prior to the
+            enrollment period, of courses they desire to take for the succeeding
+            semester. Students may enlist in classes, depending on slot
+            availability.
+          </div>
+        </AccordionComponent>
+      </div>
+      <div className="pt-[1.25rem]">
+        <AccordionComponent question="How to know the result of the GIA Application?">
+          <div>
+            Preregistration is the prelisting of students, prior to the
+            enrollment period, of courses they desire to take for the succeeding
+            semester. Students may enlist in classes, depending on slot
+            availability.
+          </div>
+        </AccordionComponent>
+      </div>
+      <div className="pt-[1.25rem]">
+        <AccordionComponent question="I am an ADDU Senior High School and have GIA, will I also apply?">
+          <div>
+            Preregistration is the prelisting of students, prior to the
+            enrollment period, of courses they desire to take for the succeeding
+            semester. Students may enlist in classes, depending on slot
+            availability.
+          </div>
+        </AccordionComponent>
+      </div>
+      <div className="pt-[1.25rem]">
+        <AccordionComponent question="How is/are grantees chosen? ">
+          <div>
+            Preregistration is the prelisting of students, prior to the
+            enrollment period, of courses they desire to take for the succeeding
+            semester. Students may enlist in classes, depending on slot
+            availability.
+          </div>
+        </AccordionComponent>
+      </div>
+      <div className="pt-[1.25rem]">
+        <AccordionComponent question="Is there a grade that Scholars need to maintain?">
+          <div>
+            Preregistration is the prelisting of students, prior to the
+            enrollment period, of courses they desire to take for the succeeding
+            semester. Students may enlist in classes, depending on slot
+            availability.
           </div>
         </AccordionComponent>
       </div>
--- src/components/ui/AccordionComponent/AccordionComponentMain.tsx
+++ src/components/ui/AccordionComponent/AccordionComponentMain.tsx
@@ -24,22 +24,16 @@ const AccordionComponent: React.FC<AccordionComponentProps> = ({
       <Accordion.Root type="single" collapsible>
         <Accordion.Item value="item-1">
           <Accordion.Trigger onClick={handleAccordionClick}>
-            <div className="w-full sm:w-[28rem] md:w-[48rem] lg:w-[64rem] xl:w-[80rem]">
-              <div className="border border-[#263167] rounded-full">
-                <div className="px-8 py-2">
-                  <div className="pl-6 flex justify-between items-center">
-                    <p className="text-[#263167] text-left">{question}</p>
-                    <div
-                      className={`pl-2 ${
-                        clickCount % 2 === 0 && clickCount > 0
-                          ? 'animate-rotateOut'
-                          : 'animate-rotateIn'
-                      }`}
-                    >
-                      <FiArrowUpRight color="#263167" />
-                    </div>
-                  </div>
-                </div>
+            <div className="w-full sm:w-[28rem] md:w-[48rem] lg:w-[64rem] xl:w-[80rem] border border-[#263167] rounded-full px-8 py-2 pl-6 flex justify-between items-center">
+              <p className="text-[#263167] text-left">{question}</p>
+              <div
+                className={`pl-2 ${
+                  clickCount % 2 === 0 && clickCount > 0
+                    ? 'animate-rotateOut'
+                    : 'animate-rotateIn'
+                }`}
+              >
+                <FiArrowUpRight color="#263167" />
               </div>
             </div>
           </Accordion.Trigger>
@@ -50,10 +44,8 @@ const AccordionComponent: React.FC<AccordionComponentProps> = ({
                 : 'animate-fadeIn'
             }`}
           >
-            <div className="w-full sm:w-[28rem] md:w-[48rem] lg:w-[64rem] xl:w-[80rem]">
-              <div className="px-10 py-6 bg-[#263167] rounded-[1.25rem]">
-                <div className="list-decimal text-white">{children}</div>
-              </div>
+            <div className="w-full sm:w-[28rem] md:w-[48rem] lg:w-[64rem] xl:w-[80rem] px-10 py-6 bg-[#263167] rounded-[1.25rem]">
+              <div className="text-white">{children}</div>
             </div>
           </Accordion.Content>
         </Accordion.Item>
