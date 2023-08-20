206c635e63e1ac9c670f47e3c5176289ad9f88ad
louispawaon <lpawaon@gmail.com>
2023-08-20 22:44:17 +0800

    refactor: added new scb header

--- src/components/SCB/SCBPage.tsx
+++ src/components/SCB/SCBPage.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 import RepresentativesSection from '@/components/SCB/RepresentativesSection';
 import SCBClickableCardsSection from '@/components/SCB/SCBClickableCardsSection';
-import SCBTitleSection from '@/components/SCB/SCBTitleSection';
 import Footer from '@/components/ui/Footer';
 import InfiniteMarquee from '@/components/ui/InfiniteMarquee';
 import Navigation from '@/components/ui/Navigation';
+import SCBHeader from '@/components/ui/SCB/SCBHeader';
 
 const SCBPage = () => {
   return (
     <>
       <Navigation />
-      <SCBTitleSection />
+      <SCBHeader />
       <SCBClickableCardsSection />
       <InfiniteMarquee />
       <RepresentativesSection />
