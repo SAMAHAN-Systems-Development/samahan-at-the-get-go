f193c1f0d895a6a0337a0544f291416cbd5134e6
louispawaon <lpawaon@gmail.com>
2023-08-20 11:33:10 +0800

    feat: recweekdeptcard section and story, initial commit

---
+++ src/app/recweek-deptcardsection/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import RecWeekDeptCardSection from '@/components/ui/RecruitmentWeek/RecWeekDeptCardSection';
+
+const page = () => {
+  return (
+    <>
+      <RecWeekDeptCardSection department="SYSDEV" />
+    </>
+  );
+};
+
+export default page;
