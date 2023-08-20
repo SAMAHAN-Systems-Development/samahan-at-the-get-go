845d90f322487aaaf6465829828552fab1ab337f
louispawaon <lpawaon@gmail.com>
2023-08-20 12:11:56 +0800

    refactor: applied suggested changes

--- src/components/ui/RecruitmentWeek/RecWeekDeptTitle.tsx
+++ src/components/ui/RecruitmentWeek/RecWeekDeptTitle.tsx
@@ -11,12 +11,12 @@ const RecWeekDeptTitle: React.FC<RecWeekDeptTitleProps> = ({
 }) => {
   return (
     <div className="flex flex-col items-center justify-center m-4 gap-0">
-      <h2 className="text-center text-lg md:text-2xl xl:text-4xl font-bold italic font-appleGaramond text-blue">
+      <span className="text-center text-lg md:text-2xl xl:text-4xl font-bold italic font-appleGaramond text-blue">
         {topText}
-      </h2>
-      <h2 className="text-center text-2xl md:text-4xl xl:text-6xl font-medium uppercase font-artega text-blue">
+      </span>
+      <span className="text-center text-2xl md:text-4xl xl:text-6xl font-medium uppercase font-artega text-blue">
         {bottomText}
-      </h2>
+      </span>
     </div>
   );
 };
