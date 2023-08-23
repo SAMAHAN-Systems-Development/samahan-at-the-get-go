31fb6615843047562a8891c0956593f2598e0c6a
Angelli <akmalkuino@addu.edu.ph>
2023-08-23 23:01:29 +0800

    refactor: add advocasix modals section, delete scrapped component, add images for advocasix

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import AdvocasixModalsSection from '@/components/Home/AdvocasixModalsSection';
+
+export default function page() {
+  return (
+    <div className="pt-32">
+      <AdvocasixModalsSection />
+    </div>
+  );
+}
---
+++ src/components/Home/AdvocasixModalsSection.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import AdvocasixDescriptionCard from '@/components/ui/Advocasix/AdvocasixDescriptionCard';
+import AdvocasixModalUI from '@/components/ui/Advocasix/AdvocasixModalUI';
+import BigSectionUI from '@/components/ui/BigSectionUI';
+import { advocasixData } from '@/data/AdvocasixData';
+
+const AdvocasixModalsSection = () => {
+  return (
+    <div>
+      <AdvocasixDescriptionCard />
+      <BigSectionUI>
+        <div className="flex flex-col space-y-4">
+          {advocasixData.map((item, index) => (
+            <React.Fragment key={index}>
+              <AdvocasixModalUI {...item} />
+            </React.Fragment>
+          ))}
+        </div>
+      </BigSectionUI>
+    </div>
+  );
+};
+
+export default AdvocasixModalsSection;
