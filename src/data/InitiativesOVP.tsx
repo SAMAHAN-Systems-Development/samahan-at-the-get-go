6ce276284dea5a96977f53fa56fe7de15c73bf4a
Angelli <akmalkuino@addu.edu.ph>
2023-08-27 23:52:07 +0800

    refactor: content update for initiatives and advocasix

--- src/data/InitiativesOVP.tsx
+++ src/data/InitiativesOVP.tsx
@@ -1,60 +1,73 @@
 import type { ModalDataType } from 'library/types/ModalDataType';
-import placeholder from 'public/assets/images/Cunanan.jpg';
+import iHelp from 'public/assets/images/Initiatives/iHelp.png';
+import knights from 'public/assets/images/Initiatives/knights.png';
+import legisleads from 'public/assets/images/Initiatives/legisleads.png';
+import mindanaoExpo from 'public/assets/images/Initiatives/mindanaoExpo.png';
+import ovpower from 'public/assets/images/Initiatives/ovpower.png';
+import pandayan from 'public/assets/images/Initiatives/pandayan.png';
+import studentBar from 'public/assets/images/Initiatives/studentBar.png';
+import iHelp2 from 'public/assets/images/InitiativesModal/iHelp2.jpg';
+import knights2 from 'public/assets/images/InitiativesModal/knights2.jpg';
+import legisleads2 from 'public/assets/images/InitiativesModal/legisleads2.jpg';
+import mindanaoExpo2 from 'public/assets/images/InitiativesModal/mindanaoExpo2.jpg';
+import ovpower2 from 'public/assets/images/InitiativesModal/ovpower2.jpg';
+import pandayan2 from 'public/assets/images/InitiativesModal/pandayan2.jpg';
+import studentBar2 from 'public/assets/images/InitiativesModal/studentBar2.jpg';
 
 export const initiativesOVP: ModalDataType[] = [
   {
     title: 'i-Help Mindanao',
     number: '01',
-    imageUrl1: placeholder,
-    imageUrl2: placeholder,
+    imageUrl1: iHelp,
+    imageUrl2: iHelp2,
     description:
       'The SAMAHAN Disaster Response Code is the standardized internal emergency protocol for the undergraduate community written to answer to personal emergencies as well as man-made & natural disasters of our students through a web of readied disaster-response student services & communication strategies. These range from a relief goods textbot, a university emergency calltree, and disaster-preparedness kits accessible by our students.',
   },
   {
     title: 'Knights at the RoundTable',
     number: '02',
-    imageUrl1: placeholder,
-    imageUrl2: placeholder,
+    imageUrl1: knights,
+    imageUrl2: knights2,
     description:
       'AdDU SAMAHAN’s 1st Student-led Socio-Civic Commentary Series, Knights At The Roundtable, is a conversation platform that calls upon the student government alongside relevant stakeholders, organizations, and concerned sectors to engage in poignant data-driven dialogue concerning with society’s professionals & leading voices. The aim is to synthesize action points & manifestos at the studentry level concerning hot-button nationwide issues.',
   },
   {
     title: 'LegisLeads Series',
     number: '03',
-    imageUrl1: placeholder,
-    imageUrl2: placeholder,
+    imageUrl1: legisleads,
+    imageUrl2: legisleads2,
     description:
       'The SAMAHAN LegisLeads Series is a set of proposed subjects of bills & resolutions, developed through administrative collaboration & public consultation, that seeks to greenlight tangible upgrades and expedite accessibility of school services to our multi-sectors. These are our student scholars, student-athletes, women, the lgbtqia+, people of color & ethnicity, students living with disabilities, international students, and our Atenean Muslim brothers and sisters.',
   },
   {
     title: 'Mindanao Exposure Program',
     number: '04',
-    imageUrl1: placeholder,
-    imageUrl2: placeholder,
+    imageUrl1: mindanaoExpo,
+    imageUrl2: mindanaoExpo2,
     description:
       'The Mindanao Exposure Program: Youth Multisectoral Agenda is a strategically arranged formative curriculum that weaves together modular learning alongside off-campus exposure trips for students to amass meaningful encounters with people, institutions, and cultural heritages. The second leg, headed by the Office of the SAMAHAN Vice-President, focuses on the multi-sectoral agenda aimed at bringing our students to further meet local identities of Midnanao’s multisectors.',
   },
   {
     title: 'Pandayan ng Filipino 2023',
     number: '05',
-    imageUrl1: placeholder,
-    imageUrl2: placeholder,
+    imageUrl1: pandayan,
+    imageUrl2: pandayan2,
     description:
       'In pursuing our #YouthAgenda, the Pandayan ng Pilipino 2023 is set to be SAMAHAN’s Legislative Capacity Building on wheels aimed at bolstering our local youth development councils & the Sanguniang Kabataan alike to create more proactive policymakers at the barangay level through the technical learning resources available in the Student Assembly, SAMAHAN Legislative Bureau, and Administrative Offices.',
   },
   {
     title: 'The OVPower Expanson',
     number: '06',
-    imageUrl1: placeholder,
-    imageUrl2: placeholder,
+    imageUrl1: ovpower,
+    imageUrl2: ovpower2,
     description:
       'In the 3rd year of the Office of the SAMAHAN Vice-President, the priorities are clear and solid: transformative student representation, proactive legislation, multi-sectoral integration, and above all, a needs-based governance that answers & pays heed to the real-time concerns of our studentry at the grassroots level. To forge a SAMAHAN na para sa Estudyanteng-Juan, the OVPower Expansion will herald the revamping of its 2 sub-bodies and the creation of 2 new departments to maximize the OSVP’s mandate & role.',
   },
   {
     title: 'The Student Integrated Bar',
     number: '07',
-    imageUrl1: placeholder,
-    imageUrl2: placeholder,
+    imageUrl1: studentBar,
+    imageUrl2: studentBar2,
     description:
       'In collaboration with the SAMAHAN Student Court and the Student Assembly, both the judicial and legislative bodies of SAMAHAN intend to establish SAMAHAN’s pioneering legal system, the Student Integrated Bar Association. It aims to ensure the welfare & due process right of our students in the face of the law through pioneering student-lawers & the code of legal procedures.',
   },
