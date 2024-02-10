9aabee10d6fca1256c32ea97fadd8224abba6a5a
louispawaon <lpawaon@gmail.com>
2024-02-10 18:29:03 +0800

    refactor(chatplugin): trying to make true minimization for mobile -added greetingDialogDisplay

--- src/components/MessengerPlugin/ChatPlugin.tsx
+++ src/components/MessengerPlugin/ChatPlugin.tsx
@@ -7,7 +7,11 @@ const ChatPlugin = () => {
   return (
     <FacebookProvider appId="892539722880146" chatSupport>
       {/* Add the correct pageID from SAMAHAN Facebook Page */}
-      <CustomChat pageId="234793876597583" minimized={true} />
+      <CustomChat
+        pageId="234793876597583"
+        minimized={true}
+        greetingDialogDisplay="hide"
+      />
     </FacebookProvider>
   );
 };
