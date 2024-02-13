308d8eaefd12bb24227facdbf3e346069f0b2d62
louispawaon <lpawaon@gmail.com>
2024-02-13 19:43:10 +0800

    refactor(studentservicesbutton): reorganized arrow to be better looking

--- src/components/ui/HelpPortal/StudentServicesButton.tsx
+++ src/components/ui/HelpPortal/StudentServicesButton.tsx
@@ -8,12 +8,10 @@ const StudentServiceButton: React.FC<StudentServiceButtonProps> = ({
 }) => {
   return (
     <div className="flex justify-between items-center w-full border-card border-lightBlue rounded-card p-7 group hover:cursor-pointer">
-      <div className="flex flex-row items-center">
-        <p className="font-artega bold text-xs text-center lg:text-sm text-lightBlue capitalize group-hover:scale-95 transition-all ease-in-out duration-300">
-          {title}
-        </p>
-        <MdArrowOutward className="ml-5" />
-      </div>
+      <p className="font-artega bold text-xs text-center lg:text-sm text-lightBlue capitalize group-hover:scale-95 transition-all ease-in-out duration-300">
+        {title}
+      </p>
+      <MdArrowOutward className="flex justify-end" />
     </div>
   );
 };
